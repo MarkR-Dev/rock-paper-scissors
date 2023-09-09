1206bdf1e3180e032948ad806ea8e73112017aaf
Mark Rodgers <markrodgers.dev@gmail.com>
2023-09-09 16:30:29 +0100

    Add function to play one round

--- script.js
+++ script.js
@@ -18,8 +18,26 @@ function getComputerChoice(){
 function getPlayerChoice(){
     let playerChoice = prompt("Choose Rock, Paper or Scissors...");
     playerChoice = playerChoice.toLowerCase();
-    console.log(playerChoice);
+    
+    return playerChoice;
 }
 
-getComputerChoice();
-getPlayerChoice();
+function playRound(playerSelection, computerSelection){
+
+    let roundResult = "";
+
+    if(playerSelection === computerSelection){
+        roundResult = "It's a tie!";
+    }else if((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")){
+       roundResult = `You win! ${playerSelection} beats ${computerSelection}`;
+    }else{
+        roundResult = `You lose! ${playerSelection} loses to ${computerSelection}`;
+    }
+
+    return roundResult;
+}
+
+let playerSelection = getPlayerChoice();
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
