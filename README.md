4bb4768eb1bd8d96e53c1033936f5ecc3fef031a
Mark Rodgers <markrodgers.dev@gmail.com>
2023-09-09 17:08:29 +0100

    Add game function to play 5 rounds with scores

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # rock-paper-scissors
 
 This is a project for JavaScript practice, this program is a implementation of a grade-school classic "rock paper scissors". At the moment the game will be played via the browser console and will be revisited later to provide a graphical user interface.
+
+Topics covered:
+Numbers(maths methods), strings(formatting), conditionals, functions(scope), loops
--- script.js
+++ script.js
@@ -25,19 +25,48 @@ function getPlayerChoice(){
 function playRound(playerSelection, computerSelection){
 
     let roundResult = "";
-
+    
     if(playerSelection === computerSelection){
-        roundResult = "It's a tie!";
+        roundResult = "It's a tie this round!";
     }else if((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")){
-       roundResult = `You win! ${playerSelection} beats ${computerSelection}`;
+        roundResult = `You won the round! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
     }else{
-        roundResult = `You lose! ${playerSelection} loses to ${computerSelection}`;
+        roundResult = `You lost the round! ${playerSelection} loses to ${computerSelection}`;
+        computerScore++;
     }
 
     return roundResult;
 }
 
-let playerSelection = getPlayerChoice();
-let computerSelection = getComputerChoice();
+function game(){
+    
+    for (let i = 1; i <= 5; i++){
+        //let playerSelection = getPlayerChoice();
+        let playerSelection = "rock";
+        let computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+        console.log(playRound(playerSelection, computerSelection));
+        
+        console.log("Score:")
+        console.log(`Player ${playerScore} - ${computerScore} Computer`);
+        console.log("");
+    }
+
+    if(playerScore > computerScore){
+        console.log("Congrats you won the game!");
+    }else if (playerScore === computerScore){
+        console.log("You tied the game.");
+    }else {
+        console.log("Sorry you lost the game.");
+    }
+}
+
+
+let playerScore = 0;
+let computerScore = 0;
+
+game();
+
+
+
