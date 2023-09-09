b355959eed05cded1aa31fc838547b722817b7ca
Mark Rodgers <markrodgers.dev@gmail.com>
2023-09-09 15:59:44 +0100

    Add function to get the computers choice

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 "use strict"
 
-console.log("test")
+function getComputerChoice(){
+    let randomNum = Math.floor((Math.random() * 3) + 1);
+    let computerChoice = "";
+    
+    if(randomNum === 1){
+        computerChoice = "rock";
+    }else if(randomNum === 2){
+        computerChoice = "paper";
+    }else{
+        computerChoice = "scissors";
+    }
+    
+    return computerChoice;
+}
+
+getComputerChoice();
